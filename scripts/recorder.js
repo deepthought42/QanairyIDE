f81e8d72625c972a4fc88694f0db6e9a0256c948
Brandon Kindred <brandon.kindred@gmail.com>
2018-11-02 20:09:38 -0400

    converted page elemnt create button to use jquery for functionality

--- scripts/recorder.js
+++ scripts/recorder.js
@@ -2,13 +2,70 @@ let startRecording = document.getElementById('startRecording');
 let stopRecording = document.getElementById('stopRecording');
 let exportTest = document.getElementById('exportTest');
 let showPageEditPanelButton = document.getElementById('createPageButton');
-let showPageElementEditPanelButton = document.getElementById('createPathElementButton');
 let pageEditPanel = document.getElementById('pageForm');
 let pageElementEditPanel = document.getElementById('pageElementForm');
-let pathElementRowEventBindings = $('#test_path_viewer').on("click", "div", function(){
+
+
+$('#createPageElementButton').on("click", function(){
+  $('#pageElementXpath').val("");
+  $('actionName').val("");
+  $('#actionValue').val("");
+  $('#pageElementIndexInPath').val("");
+
+  console.log("show page element edit panel");
+  pageEditPanel.style.display = "none";
+  pageElementEditPanel.style.display = "block";
+  $('#pageElementIndexInPath').val("");
+});
+
+/*saves a page element by either updateing it if the index is set, otherwise
+ * creating a new page element action set and appending the set to the end of
+ * the path stored in localhost
+*/
+$('#savePageElement').on("click", function(){
+  var element_action = {
+    element: {
+      xpath: $('#pageElementXpath').val()
+    },
+    action :{
+      name : $('actionName').val(),
+      value: $('#actionValue').val()
+    }
+  }
+
+  var path = JSON.parse(localStorage.path);
+  console.log("Path before :: "+path.length);
+  var index = $('#pageElementIndexInPath').val();
+  if(index && index.length > 0){
+    path[ index ] = element_action;
+    console.log("updated element at :: "+index);
+  }
+  else {
+    path.push(element_action);
+  }
+  localStorage.setItem('path', JSON.stringify(path));
+  console.log("path after  ::  "+path.length);
+
+});
+
+let pathElementRowEventBindings = $('#test_path_viewer').on("click", ".path-element", function(){
     //send element to path element form
     var index = $(this).data("index");
     console.log("Clicked path element "+index);
+
+    var element = JSON.parse(localStorage.getItem("path"))[index];
+    console.log("Path element :: "+element);
+    console.log("PATH INDEX :: "+index);
+
+    console.log("Path element :: "+ JSON.stringify(element));
+
+    if(element.element){
+      //send to path element form
+      $('#pageElementXpath').val(element.element.xpath);
+      $('actionName').val(element.action.name);
+      $('#actionValue').val(element.action.value);
+      $('#pageElementIndexInPath').val(index);
+    }
   });
 
 pageEditPanel.style.display = "none";
@@ -57,16 +114,46 @@ showPageEditPanelButton.onclick = function(page){
   pageElementEditPanel.style.display = "none";
 }
 
-showPageElementEditPanelButton.onclick = function(element){
-  console.log("show page element edit panel");
-  pageEditPanel.style.display = "none";
-  pageElementEditPanel.style.display = "block";
-}
-
-
 let editPathElement = function(element_idx){
   console.log("editing path element  ::  " + element);
   PubSub.publish('edit-path-element', element);
+}
+
+let generatePageElementPathListItem = function(path_element, index){
+  var element=  `
+  <div  class='row path-element' data-index="` + index + `">
+    <div class="col-xs-10">
+      <div class="element col-xs-12">
+        <div class='col-xs-3 path-element-type'>
+          xpath
+        </div>
+        <div class='col-xs-9 path-element-value'>`
+          + path_element.element.xpath +`
+        </div>
+      </div>
+      <div class="action col-xs-12">
+        <div class='col-xs-3 path-element-type'>
+          `+ path_element.action.name + `
+        </div>
+        <div class='col-xs-9 path-element-value' >`
+          + path_element.action.value + `
+        </div>
+      </div>
+    </div>
+    <div class='col-xs-2 icons' >
+      <i class='fa fa-pencil icon' onClick={this.editPathElement.bind(this, item)}></i>
+      <i class='fa fa-times icon delete-icon' onClick={this.removePathElement.bind(this, item)}></i>
+    </div>
+  </div>`;
+     //  To do something
+
+     return element;
+}
+
+let redrawPathElement = function(element, index){
+  var list_item_html = generatePageElementPathListItem(element, indexz);
+  $('#test_path_viewer').children().get(index).html(element);
+
 }
 
 //receive path element
@@ -82,18 +169,21 @@ chrome.runtime.onMessage.addListener(
             localStorage.setItem("path", JSON.stringify(path));
             console.log("pre path :: "+path);
             console.log("pre path size :: "+path.length);
-
           }
 
+          console.log("Path length "+path.length);
           if(path.length === 0){
             //push page into path
+            path.push({url : "testurl.com"});
           }
 
+          console.log("Path length after adding page :: "+path.length);
           //check if last element is equal to this element
-          console.log("path[path.length-1].element.xpath  :   "+path[path.length-1].element.xpath );
-          console.log("request.data.element.xpath  ::   "+request.data.element.xpath);
-          if(path[path.length-1].element.xpath == request.data.element.xpath){
+          if(path[path.length-1].element && path[path.length-1].element.xpath == request.data.element.xpath){
             console.log("elements match");
+            console.log("path[path.length-1].element.xpath  :   "+path[path.length-1].element.xpath );
+            console.log("request.data.element.xpath  ::   "+request.data.element.xpath);
+
             return;
           }
 
@@ -102,44 +192,9 @@ chrome.runtime.onMessage.addListener(
 
           console.log("path size :: "+path.length);
           console.log("PATH :: "+JSON.stringify(path));
-         var element=  `
-         <div  class='row path-element' data-index="` + $('.path-element').length + `">
-           <div class="col-xs-10">
-             <div class="element col-xs-12">
-               <div class='col-xs-3 path-element-type'>
-                 xpath
-               </div>
-               <div class='col-xs-9 path-element-value'>
-               </div>
-             </div>
-             <div class="action col-xs-12">
-               <div class='col-xs-3 path-element-type'>
-                 click
-               </div>
-               <div class='col-xs-9 path-element-value'>
-                 none
-               </div>
-             </div>
-           </div>
-           <div class='col-xs-2 icons' >
-             <i class='fa fa-pencil icon' onClick={this.editPathElement.bind(this, item)}></i>
-             <i class='fa fa-times icon delete-icon' onClick={this.removePathElement.bind(this, item)}></i>
-           </div>
-         </div>`;
-            //  To do something
+          console.log("REQUEST DATA FOR ADDING TO PATH :: " + request.data);
 
-            console.log("REQUEST DATA FOR ADDING TO PATH :: " + request.data);
-            var path_element = "";
-            if(request.data.type == 'page'){
-              path_element = "<div class='element-action node-group vertical__center__int'>url : "+ request.data.url+"</div>";
-            }
-            else if(request.data.type == 'pageElement'){
-              path_element = "<div class='element-action node-group vertical__center__int'> element coordinates : ("+ request.data.client_x+" : " +request.data.client_y+")</div>";
-            }
-            else if(request.data.type == 'action'){
-              path_element = "<div class='element-action node-group vertical__center__int'>action : "+request.data.name+"</div>";
-            }
-            $('#test_path_viewer').append(element);
+          $('#test_path_viewer').append( generatePageElementPathListItem(request.data, path.length-1 ));
         }
-    }
+      }
 );
