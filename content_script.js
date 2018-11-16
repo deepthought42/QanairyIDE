671e82ba85c6b7eb217a6491903165f974d1c26a
Brandon Kindred <brandon.kindred@gmail.com>
2018-11-16 08:16:06 -0500

    fixed draggable issue

--- content_script.js
+++ content_script.js
@@ -9,7 +9,7 @@ iframe.src = chrome.extension.getURL('/recorder.html');
 
 
 var header = document.createElement("div");
-header.style.cssText = 'width:100%;height:20px';
+header.style.cssText = 'width:250px;height:20px;z-index:100;background-color:white';
 header.id="qanairy_ide_header";
 
 var body = document.createElement("div");
@@ -43,9 +43,26 @@ let recorder_event_listener = function(event){
 
   //iframe.contentWindow.postMessage({element: {type: "element", xpath: xpath}, action: {type: "action", name: "click", value:""}}, "http://localhost:3000");
 
-  chrome.runtime.sendMessage({msg: "addToPath", data: {url: window.location.toString(), pathElement: { element: {type: "pageElement", target: event.relatedTarget, xpath: xpath}, action: {type: "action", name: "click", value: ""}}}}, function(response) {
-    console.log("response ::  " +JSON.stringify(response));
-  });
+  chrome.runtime.sendMessage({msg: "addToPath",
+                              data: {url: window.location.toString(),
+                                     pathElement: {
+                                       element: {
+                                         type: "pageElement",
+                                         target: event.relatedTarget,
+                                         xpath: xpath
+                                       },
+                                       action: {
+                                         type: "action",
+                                         name: "click",
+                                         value: ""
+                                       }
+                                     }
+                                   }
+                                 },
+     function(response) {
+       console.log("response ::  " +JSON.stringify(response));
+     }
+   );
 }
 
 
@@ -130,6 +147,34 @@ let generateXpath = function(elem){
         break;
       }
     }
+    if(attributes_check.length > 0){
+      xpath += "[";
+      for(var idx=0; idx < attributes_check.length; idx++){
+        xpath += attributes_check[idx];
+        if(idx < attributes_check.length-1){
+          xpath += " and ";
+        }
+      }
+      xpath += "]";
+    }
+
+    var elements = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+
+    if(elements.length > 1){
+      var count = 1;
+      for(var idx=0; idx < elements.length; idx++){
+        if(elements[idx].getTagName().equals(elem.getTagName())
+            && elements[idx].getText().equals(elem.getText())){
+              xpath += ("("+xpath+")[" + count + "]");
+        }
+        count++;
+      }
+    }
+
+  //  console.log("xpath  :::   "+xpath);
+
+    return xpath;
+  }
 }
 
 
@@ -169,9 +214,9 @@ function dragElement(elmnt) {
     document.onmousemove = elementDrag;
   }
 
-  if (document.getElementById(elmnt.id + "_header")) {
+  if (document.getElementById(elmnt.id +"_header")) {
     // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "_header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id +"_header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
     elmnt.onmousedown = dragMouseDown;
@@ -182,90 +227,6 @@ function dragElement(elmnt) {
 // Make the DIV element draggable:
 dragElement(document.getElementById("qanairy_ide"));
 
-  if(attributes_check.length > 0){
-    xpath += "[";
-    for(var idx=0; idx < attributes_check.length; idx++){
-      xpath += attributes_check[idx];
-      if(idx < attributes_check.length-1){
-        xpath += " and ";
-      }
-    }
-    xpath += "]";
-  }
-
-  var elements = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-
-  if(elements.length > 1){
-    var count = 1;
-    for(var idx=0; idx < elements.length; idx++){
-      if(elements[idx].getTagName().equals(elem.getTagName())
-          && elements[idx].getText().equals(elem.getText())){
-            xpath += ("("+xpath+")[" + count + "]");
-      }
-      count++;
-    }
-  }
-
-//  console.log("xpath  :::   "+xpath);
-
-  return xpath;
-}
-
-/**
-	 * generates a unique xpath for this element.
-	 *
-	 * @return an xpath that identifies this element uniquely
-	 */
-   /*
-let generateXpath = function(WebElement element, String xpath, Map<String, Integer> xpathHash, WebDriver driver, Set<Attribute> attributes){
-		ArrayList<String> attributeChecks = new ArrayList<String>();
-
-		xpath += "//"+element.getTagName();
-		for(Attribute attr : attributes){
-			if(Arrays.asList(valid_xpath_attributes).contains(attr.getName())){
-
-				String attribute_values = ArrayUtility.joinArray(attr.getVals().toArray(new String[attr.getVals().size()]));
-				if(attribute_values.contains("\"")){
-					attributeChecks.add("contains(@" + attr.getName() + ",\"" +generateConcatForXPath(attribute_values.trim())+ "\")");
-				}
-				else{
-					attributeChecks.add("contains(@" + attr.getName() + ",\"" + escapeQuotes(attribute_values.trim()) + "\")");
-				}
-			}
-		}
-		if(attributeChecks.size()>0){
-			xpath += "[";
-			for(int i = 0; i < attributeChecks.size(); i++){
-				xpath += attributeChecks.get(i).toString();
-				if(i < attributeChecks.size()-1){
-					xpath += " and ";
-				}
-			}
-			xpath += "]";
-		}
-
-	    WebElement parent = element;
-	    int count = 0;
-	    while(!parent.getTagName().equals("html") && !parent.getTagName().equals("body") && parent !== null && count < 4){
-	    	try{
-	    		parent = getParentElement(parent);
-	    		if(driver.findElements(By.xpath("//"+parent.getTagName() + xpath)).size() == 1){
-	    			return "//"+parent.getTagName() + xpath;
-	    		}
-	    		else{
-		    		xpath = "/" + parent.getTagName() + xpath;
-	    		}
-	    	}catch(InvalidSelectorException e){
-	    		parent = null;
-	    		log.error("Invalid selector exception occurred while generating xpath through parent nodes");
-	    		break;
-	    	}
-	    	count++;
-	    }
-	    xpath = "/"+xpath;
-		return uniqifyXpath(element, xpathHash, xpath, driver);
-	}
-*/
   /**
 	 * creates a unique xpath based on a given hash of xpaths
 	 *
