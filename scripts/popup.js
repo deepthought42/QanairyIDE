8a208a69943dff687d42e0228887be95e117ff96
Brandon Kindred <brandon.kindred@gmail.com>
2019-01-17 07:38:51 -0500

    added auth0 lock to plugin popup for login

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,34 +1,29 @@
 
-  var auth0 = new auth0.WebAuth({
-    domain: "staging-qanairy.auth0.com",
-    clientID:  "mMomHg1ZhzZkM4Tsz2NGkdJH3eeJqIq6",//"wT7Phjs9BpwEfnZeFLvK1hwHWP2kU7LV",
-    responseType: "token id_token",
-    audience: "https://staging-api.qanairy.com",
-    scope: "openid profile email read:domains delete:domains update:domains create:domains create:accounts read:accounts delete:accounts update:accounts read:tests update:tests read:groups update:groups create:groups delete:groups run:tests start:discovery read:actions"
+  var lock = new Auth0Lock(
+    'mMomHg1ZhzZkM4Tsz2NGkdJH3eeJqIq6',
+    'staging-qanairy.auth0.com', {
+    auth: {
+      responseType: 'code',
+      params: {
+        scope: 'openid email' // Learn about scopes: https://auth0.com/docs/scopes
+      }
+    }
   });
 
-
-  $("#login").on('click',  function(){
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://staging-qanairy.auth0.com/dbconnections/login",
-      "method": "POST",
-      "headers": {
-        "content-type": "application/x-www-form-urlencoded"
-      },
-      "data": {
-        "client_id": "mMomHg1ZhzZkM4Tsz2NGkdJH3eeJqIq6",
-        "email": $('#login_email').val(),
-        "password": $('#login_password').val(),
-        "connection": "Username-Password-Authentication"
+  // Listening for the authenticated event
+  lock.on("authenticated", function(authResult) {
+    // Use the token in authResult to getUserInfo() and save it to localStorage
+    lock.getUserInfo(authResult.accessToken, function(error, profile) {
+      if (error) {
+        // Handle error
+        return;
       }
-    };
 
-    auth0.loginAndAuthorize(settings.data, function(err, res){
-      //console.log("response :: "+err + "   :    " + Object.keys(res));
-      if(!err){
+      document.getElementById('nick').textContent = profile.nickname;
 
-      }
+      localStorage.setItem('accessToken', authResult.accessToken);
+      localStorage.setItem('profile', JSON.stringify(profile));
     });
   });
+
+  lock.show();
