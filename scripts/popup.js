4da3265f7f72b07a70df70476d13a46a4d33afc9
Brandon Kindred <brandon.kindred@gmail.com>
2019-02-22 22:08:23 -0500

    removed logging statements

--- scripts/popup.js
+++ scripts/popup.js
@@ -52,7 +52,6 @@
       if (token && isLoggedIn(token)) {
         renderProfileView(authResult);
 
-        console.log("authenticated");
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
           chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box", msg: "open_recorder"}, function(response) {});
         });
@@ -62,7 +61,6 @@
           });
 
       } else {
-        console.log("sending authenticate call");
         renderDefaultView();
       }
     }
